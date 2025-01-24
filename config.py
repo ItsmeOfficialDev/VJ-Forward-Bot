fd09eb6e807c487d087794b92cd2490cc56d984e
Devaduttjr <devaduttjr@gmail.com>
2025-01-24 17:24:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ from os import environ
 class Config:
     API_ID = int(environ.get("API_ID", "21630936"))
     API_HASH = environ.get("API_HASH", "d39af8130ac213646bb3b4b824c35e4b")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "8053823493:AAFGJYW1Wx6_yAlJYaUyCT7Pd7b8RIREVbs") 
+    BOT_TOKEN = environ.get("BOT_TOKEN", "") 
     BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
     DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://Telegram:Telegram@cluster0.xfq93.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "vj-forward-bot")
